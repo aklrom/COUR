60a775e88364e5b06127e55fe0223131ebb09e97
aklrom <aklekpessou@gmail.com>
2026-08-14 11:17:54 +0100

    mis en place de la logique js pour recuperer les données

--- joke.js
+++ joke.js
@@ -2,8 +2,24 @@ let jokescreen = document.getElementById("jokescreen");
 let btn = document.querySelector("button");
 btn.addEventListener("click", async (e) => {
   e.preventDefault();
-  let url = "";
-  let response = await fetch(url);
-  let joke = (await response).json();
-  console.log(joke);
+  let url = "https://api.api-ninjas.com/v1/jokes";
+  let apiKey= "XAi2qxwiBZcSSDdj6fpMjm5jPpVkFbLpXInhU5cG";
+  try{
+    let Response=await fetch(url,{
+      method:'GET',
+      headers:{
+        'X-Api-Key':apiKey,
+        'Content-Type':'application/json'
+      }
+    });
+    if(!Response.ok){
+      throw new Error(`Erreur HTTP : ${Response.status}`);
+    }
+
+    const data=await Response.json();
+    console.log(data[0].joke);
+    jokescreen.innerHTML=`<p>${data[0].joke}</p>`
+  }catch (error){
+    alert("Quelque chose s'est mal passé.Reéssayer");
+  }
 });
